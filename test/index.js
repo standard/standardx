5f22ccbf6c986f3ca50c062cd5e243f6068f5419
Dan Flettre <flettre@gmail.com>
2018-05-18 16:29:32 -0500

    add tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,35 @@
+var path = require('path')
 var test = require('tape')
+var crossSpawn = require('cross-spawn')
 
-test('Example Test', function (t) {
+var CMD_PATH = path.join(__dirname, '..', 'bin', 'cmd.js')
+
+test('command line usage: --help', function (t) {
   t.plan(1)
-  t.pass('No tests defined.')
+
+  var result = crossSpawn.sync(CMD_PATH, ['--help'])
+  if (result.error) t.fail(result.error)
+  t.equal(result.status, 0, 'zero exit code')
+})
+
+test('test-repo-eslintrc allows snake_case', function (t) {
+  t.plan(3)
+
+  var result = crossSpawn.sync(CMD_PATH, [path.join(__dirname, 'test-repo-eslintrc', 'index.js')])
+  if (result.error) t.fail(result.error)
+  t.equal(result.status, 0, 'zero exit code')
+
+  t.equal(result.stdout.toString().trim(), '', 'no output')
+  t.equal(result.stderr.toString().trim(), '', 'no error output')
+})
+
+test('test-repo-package-json allows snake_case', function (t) {
+  t.plan(3)
+
+  var result = crossSpawn.sync(CMD_PATH, [path.join(__dirname, 'test-repo-package-json', 'index.js')])
+  if (result.error) t.fail(result.error)
+  t.equal(result.status, 0, 'zero exit code')
+
+  t.equal(result.stdout.toString().trim(), '', 'no output')
+  t.equal(result.stderr.toString().trim(), '', 'no error output')
 })
