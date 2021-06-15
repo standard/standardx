a261ab881f55c6845720f950451fe5e22d7bbdcf
Dan Flettre <flettre@gmail.com>
2021-06-15 18:35:19 -0500

    resolve eslint config and plugins manually fixes #27

--- options.js
+++ options.js
@@ -2,12 +2,21 @@ const pkg = require('./package.json')
 const stdVersion = require('standard/package.json').version
 const stdOpts = require('standard/options.js')
 
+const standardConf = require('standard/eslintrc.json')
+
+const resolvedConf = {
+  extends: standardConf.extends.map(function (configName) {
+    return require.resolve(`eslint-config-${configName}`)
+  })
+}
+
 const opts = Object.assign({}, stdOpts, {
   bugs: pkg.bugs.url,
   cmd: 'standardx',
   eslintConfig: {
-    baseConfig: require('standard/eslintrc.json'),
-    useEslintrc: true
+    baseConfig: resolvedConf,
+    useEslintrc: true,
+    resolvePluginsRelativeTo: __dirname
   },
   homepage: pkg.homepage,
   tagline: 'Use JavaScript Standard Style (tweaked by standardx)',
